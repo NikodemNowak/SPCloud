541a243c5c35f176ac17cef0bedea6b902030011
MateuszKosowski <mateuszkosowski03@gmail.com>
2025-10-12 17:11:27 +0200

    Comments + delete create_tables.py

--- backend/app/init_db.py
+++ backend/app/init_db.py
@@ -7,13 +7,18 @@ from models.models import Base
 
 def init_db():
 
+    # Download DATABASE_URL from environment variable or use a default value
     DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:secret@db:5432/mydb")
     print(f"Connecting to db: {DATABASE_URL}")
 
-    # Object to communicate with the database
+    # Creates a database engine.
+    # This is the object that manages database connections. It is configured via a URL.
     engine = create_engine(DATABASE_URL)
 
     print("Adding all tables to the database...")
+    # `Base.metadata` is an object containing the schema of all tables defined in models (everything that inherits from `Base`).
+    # `.create_all(engine)` is a method that says: "Iterate through all tables
+    # in this schema and for each one send a CREATE TABLE command to the database (via `engine`), but only IF that table does not already exist."
     Base.metadata.create_all(engine)
     print("tables created successfully.")
 
--- backend/app/main.py
+++ backend/app/main.py
@@ -3,19 +3,22 @@ from contextlib import asynccontextmanager
 from api.v1.api import api_router
 from init_db import init_db
 
-
+# Sepecial object to manage the lifespan of the app
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     print("Initializing application...")
+    # Initialize the database
     init_db()
-    yield # yield is used to separate startup and shutdown code
+    # yield is used to separate startup and shutdown code
+    yield
     print("Shutting down application...")
 
-
+# Creating the FastAPI app
 app = FastAPI(
     title="SPCloud API",
     description="API to manage files in SPCloud",
     version="1.0.0",
+    # We tell the FastAPI instance to use our `lifespan` function to manage its lifecycle.
     lifespan=lifespan
 )
 
@@ -25,5 +28,7 @@ async def root():
     return {"status": "ok", "message": "Hello World"}
 
 
-# Turning on the API router
+# We include in our main `app` application all the endpoints defined
+# in the `api_router` object that we imported at the beginning.
+# This way, all the routes defined in `api_router` will be available under the `/api/v1` prefix.
 app.include_router(api_router, prefix="/api/v1")
